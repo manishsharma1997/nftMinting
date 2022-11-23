ef1562381236c0b59e6aa8dd99123e4dd6af21fc
manishsharma1997 <ms097ms@gmail.com>
2022-11-23 18:47:28 +0530

    21th Commit

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import ContextProvider from './utils/ContextProvider';
+import {BrowserRouter} from "react-router-dom"
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <ContextProvider>
+  <BrowserRouter>
     <App />
+    </BrowserRouter>
     </ContextProvider>
 );
 
